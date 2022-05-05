1ed15cabb1a0a7099591394eafba596b39e28540
Kevin Stradtman <webkit.development.systems@gmail.com>
2022-05-04 21:44:19 -0400

    Added fuctionality to add 2 different dice when clicking one of the buttons

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -16,19 +18,56 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  const DicePage({Key key}) : super(key: key);
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int lDiceNumber = 1;
+  int rDiceNumber = 1;
+
+  void changeDiceFace() {
+    setState(() {
+      lDiceNumber = Random().nextInt(6) + 1;
+      rDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: <Widget>[
-        Expanded(
-          child: Image.asset('images/dice1.png')
-        ),
-        Expanded(
-          child: Image.asset('images/dice1.png'),
-        ),
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                setState(() {
+                  changeDiceFace();
+                });
+              },
+              child: Image.asset('images/dice$lDiceNumber.png'),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                setState(() {
+                  changeDiceFace();
+                });
+              },
+              child: Image.asset('images/dice$rDiceNumber.png'),
+            ),
+          ),
 
-      ],
+        ],
+      ),
     );
   }
 }
+
+
+
+
